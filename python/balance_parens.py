27f37b7005dbd57cbee349dfca2b3c8d4f12fb05
Kate Kim <katehrkim@gmail.com>
2021-05-27 20:19:15 -0400

    Kate's answer

--- python/balance_parens.py
+++ python/balance_parens.py
@@ -1,2 +1,35 @@
 def balance_parens(str):
-    pass
+    output = ''
+    state = 'not_found'
+    bool_list = [None] * len(str)
+    for i in range(0, len(str)):
+        if state == 'not_found':
+            if str[i] == '(':
+                state = 'found_left'
+                bool_list[i] = 'maybe'
+            elif str[i] == ')':
+                bool_list[i] = False
+            else:
+                bool_list[i] = True
+        elif state == 'found_left':
+            if str[i] == '(':
+                state = 'found_left'
+                bool_list[i] = 'maybe'
+            elif str[i] == ')':
+                bool_list[i] = True
+                j = i
+                while (bool_list[j] != 'maybe'):
+                    bool_list[j] = True
+                    j = j - 1
+                bool_list[j] = True
+                state = 'not_found'
+            else:
+                bool_list[i] = True
+    for i in range(0, len(bool_list)):
+        if bool_list[i] == True:
+            output = output + str[i]
+    return output
+
+    # Did not do challenge
+
+
--- python/balance_parens_spec.py
+++ python/balance_parens_spec.py
@@ -1,8 +1,18 @@
+import unittest
 from balance_parens import balance_parens
 
 # Add more test cases!...
-print(balance_parens("abc(d)e(fgh))(i)j)k") == "abc(d)e(fgh)(i)jk")
-print(balance_parens("abc((d)e(fgh)(i)j(k") == "abc(d)e(fgh)(i)jk")
+class BalanceParensTestCase(unittest.TestCase):
+    """Tests for balance_parens.py"""
+    def test_one(self):
+        self.assertEqual(balance_parens("abc(d)e(fgh))(i)j)k"), "abc(d)e(fgh)(i)jk")
+    def test_two(self):
+        self.assertEqual(balance_parens("abc((d)e(fgh)(i)j(k"), "abc(d)e(fgh)(i)jk")
+    # Challenge: nested parentheses...
+    def test_three(self):
+        self.assertEqual(balance_parens
+        ("abc(d)(ef(g(h))ij)k)lm()o)p"), 
+        "abc(d)(ef(g(h))ij)klm()op")
 
-# Challenge: nested parentheses...
-print(balance_parens("abc(d)(ef(g(h))ij)k)lm()o)p") == "abc(d)(ef(g(h))ij)klm()op")
+if __name__ == '__main__':
+    unittest.main()
